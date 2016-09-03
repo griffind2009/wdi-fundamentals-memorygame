cf1cd085a98d44dc4ac3a691a2a8706a706ad917
Domonique Griffin <griffind2009@gmail.com>
2016-09-02 22:23:31 -0400

    Added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,11 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+if (cardThree === cardFour) {
+	alert('You found a match');
+}
+else {
+	alert('Sorry try agian!');
+}
